176b996a01343afc789db6fe2c846e3835806a33
lucas <lucas.peralta@gmail.com>
2017-06-25 01:10:34 -0300

    initial commit

---
+++ js/estudio.js
@@ -0,0 +1,32 @@
+
+(function($) {
+  
+
+    // Animacion para scrollear cuando hago click en el menu y voy a una seccion
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+ 
+
+   // Agrego a la barra de navegacion la clase affix para que scrolee junto con la pagina
+    // ademas estan definidos el csscon lo cual se agrega el efecto de cambiar de color de fondo
+    //de la barra de navegacion
+   
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+     // Cierro el menu despues de elegir una opcion (modo celular )
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+    
+    
+})(jQuery); 
